577cc19e7edc57d317fc4483ddd9c6e9d085ea5a
Cami Anderson <camidantas@icloud.com>
2022-04-07 16:25:41 -0400

    Add random recipe functionality

--- main.js
+++ main.js
@@ -3,9 +3,21 @@
 var buttonLetsCook = document.querySelector(".letscook");
 var cookpot = document.querySelector(".cookpot");
 var youShouldMake = document.querySelector(".you-should-make");
+var foodSuggestion = document.querySelector(".result");
 
 //event listeners
 buttonLetsCook.addEventListener("click", showRecipe);
+
+//recipes
+var sides = ["fries", "beans", "rice", "salad", "baked potato"];
+var mainDishes = ["Strogonoff", "Hamburger", "Pasta", "Tacos", "Pizza"];
+var desserts = ["Ice Cream", "Pudim", "Black Forest Cake", "Cupcake"];
+
+
+var sideOption = document.querySelector('input[value="side"]');
+var mainOption = document.querySelector('input[value="main-dish"]');
+var dessertOption = document.querySelector('input[value="dessert"]');
+var entireMealOption = document.querySelector('input[value="entire-meal"]');
 
 
 // functions
@@ -18,3 +30,17 @@ function hideCookpot(){
   cookpot.classList.add("hidden");
   youShouldMake.classList.remove("hidden");
   }
+
+function displayDishes() {
+  if(sideOption.checked){
+    foodSuggestion.innerText = sides[getRandomIndex(sides)];
+  } else if (mainOption.checked){
+    foodSuggestion.innerText = mainDishes[getRandomIndex(mainDishes)]
+  } else if (dessertOption.checked){
+    oodSuggestion.innerText = desserts[getRandomIndex(desserts)]
+  }
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
