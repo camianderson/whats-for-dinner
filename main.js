72da98cf72bd288855048a05b0d758661435f385
Cami Anderson <camidantas@icloud.com>
2022-04-08 17:24:49 -0400

    Add functionality to the entire meal option

--- main.js
+++ main.js
@@ -9,9 +9,9 @@ var foodSuggestion = document.querySelector(".result");
 buttonLetsCook.addEventListener("click", showRecipe);
 
 //recipes
-var sides = ["fries", "beans", "rice", "salad", "baked potato"];
-var mainDishes = ["Strogonoff", "Hamburger", "Pasta", "Tacos", "Pizza"];
-var desserts = ["Ice Cream", "Pudim", "Black Forest Cake", "Cupcake"];
+var sides = ["Baked Pineapple", "Smashed Cauliflower", "Colorful Spinach and Prosciutto", "Sauteed Summer Squash", "Spicy Bean and Corn Salsa", "Kale and Mushroom", "Baked Potato", "Seasonal Veggies", "Corn Muffin", "Scalloped Potatos"];
+var mainDishes = ["Chicken Parmesan", "Sicilian Roasted Chicken", "Spaghetti Carbonara", "Dumplings", "Reuben Sandwich", "German Hamburgers", "Chicken Pot Pie", "Braised Corned Beef Brisket", "Cilantro-Lime Baked Shrimp", "Shoyu Pork", "Salmon Patties", "Ranch Pork Chops", "Avocado Tacos", "Spinach Lasagna", "Vegetarian Meatloaf"];
+var desserts = ["Ice Cream", "Tiramisu Toffee", "Strawberry Pudding", "Pumpkin Pie", "Chocolate Lava Cake", "Key Lime Pie", "Cheesecake",];
 var meal = [];
 
 var foodArray = {
@@ -40,6 +40,11 @@ function getRandomIndex(array) {
 function displayDishes() {
   var radio = document.querySelector('input[type=radio]:checked');
   var randomMeal = getRandomIndex(foodArray[radio.value]);
-  foodSuggestion.innerText = `${foodArray[radio.value][randomMeal]}`;
-
+  if( radio.value === 'meal'){
+    foodSuggestion.innerText = `${foodArray["main"][randomMeal]} with a side of `
+    foodSuggestion.innerText += ` ${foodArray["sides"][randomMeal]} `
+    foodSuggestion.innerText += ` and ${foodArray["dessert"][randomMeal]} for dessert!`
+  } else {
+    foodSuggestion.innerText = `${foodArray[radio.value][randomMeal]}`;
+  }
 }
